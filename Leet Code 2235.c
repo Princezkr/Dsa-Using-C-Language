4ac4ea6c8e7e12cef799c62821cffa1ee287bab1
Karmajoty Biswal <150656048+Princezkr@users.noreply.github.com>
2026-01-07 15:01:00 +0530

    Add Two Integers.
    Solution Done :)

--- Leet Code 2235.c
+++ Leet Code 2235.c
@@ -1,4 +1,6 @@
+// This program takes two integers as input from the user and calculates their sum.
 #include<stdio.h>
+#include<conio.h>
 int main(){
     int Num1,Num2,Sum;
     printf("Enter Num1: ");
@@ -7,4 +9,5 @@ int main(){
     scanf("%d",&Num2);
     Sum=Num1+Num2;
     printf("Sum Of The Numbers is: %d",Sum);
+    getch();
 }
